7efc91524455256e38b11f3af1b60a15fac0078d
unknown <costa.wesley@icloud.com>
2022-05-31 16:35:41 -0300

    schema atualizado

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,7 +5,7 @@ const livroSchema = new mongoose.Schema(
         id: {type: String},
         titulo: {type: String, required: true},
         autor: {type: String, required: true},
-        NumPaginas: {type: Number}
+        
     }
 );
 
